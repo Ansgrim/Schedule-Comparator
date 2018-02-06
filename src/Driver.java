769ecbb2956b83dcdc243a55aa81886fa0ddb318
Michael Wittman <michaelwittman290@gmail.com>
2018-02-06 14:30:18 -0600

    Added UI and Driver detail
    Main method constructs and uses the set methods for the Control and UI classes. Added a basic constructor and set method to the UI

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,11 @@
+import javax.swing.JFrame;
 
 public class Driver {
 	public void main (String[] args) {
-
+			Control c = new Control();
+			UI gui = new UI();
+			//c.setUI(gui);
+			gui.setControl(c);
+			gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
--- src/UI.java
+++ src/UI.java
@@ -1,4 +1,22 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class UI {
+@SuppressWarnings("serial")
+public class UI extends JFrame{
 
+	private Control c;
+	
+	public UI(){
+		JPanel	panel = new JPanel();
+
+		this.getContentPane().add(panel);
+		this.setSize(500, 500);;
+		this.validate();
+		
+		this.setVisible(true);
+	}
+	public void setControl(Control c) {
+		this.c = c;
+	}
+		
 }
